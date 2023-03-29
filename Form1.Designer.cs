f00317eeff7529e41f696d9c9d20c897abb4f5a3
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2023-03-29 20:12:40 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace LOGIN_E_SENHA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxLogin = new System.Windows.Forms.TextBox();
+            this.txtBoxSenha = new System.Windows.Forms.TextBox();
+            this.btnOk_Click = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SENHA";
+            // 
+            // txtBoxLogin
+            // 
+            this.txtBoxLogin.Location = new System.Drawing.Point(147, 58);
+            this.txtBoxLogin.Name = "txtBoxLogin";
+            this.txtBoxLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxLogin.TabIndex = 2;
+            // 
+            // txtBoxSenha
+            // 
+            this.txtBoxSenha.Location = new System.Drawing.Point(147, 99);
+            this.txtBoxSenha.Name = "txtBoxSenha";
+            this.txtBoxSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxSenha.TabIndex = 3;
+            // 
+            // btnOk_Click
+            // 
+            this.btnOk_Click.Location = new System.Drawing.Point(65, 156);
+            this.btnOk_Click.Name = "btnOk_Click";
+            this.btnOk_Click.Size = new System.Drawing.Size(75, 23);
+            this.btnOk_Click.TabIndex = 4;
+            this.btnOk_Click.Text = "LOGIN";
+            this.btnOk_Click.UseVisualStyleBackColor = true;
+            this.btnOk_Click.Click += new System.EventHandler(this.btnOk_Click_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(172, 156);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LOGIN_E_SENHA.Properties.Resources.MEGAN_FOX;
+            this.pictureBox1.Location = new System.Drawing.Point(296, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 85);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(450, 225);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOk_Click);
+            this.Controls.Add(this.txtBoxSenha);
+            this.Controls.Add(this.txtBoxLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxLogin;
+        private System.Windows.Forms.TextBox txtBoxSenha;
+        private System.Windows.Forms.Button btnOk_Click;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LOGIN_E_SENHA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnOk_Click_Click(object sender, EventArgs e)
+        {
+            if (txtBoxLogin.Text == "Admin" && txtBoxSenha.Text == "123456")
+            {
+                MessageBox.Show("Bem vindo ao sistema!", "Acesso ao sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else 
+            {
+                MessageBox.Show("Acesso Negado!", "Acesso Negado!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+    }
+}
